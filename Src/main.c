6db5cbc04b6a21837c3e1ec1328597d63ba984d7
chetsala <chetsala@gmail.com>
2024-10-24 22:44:58 -0400

    Divided output buffer by 12 to reduce output voltage

--- Src/main.c
+++ Src/main.c
@@ -566,7 +566,7 @@ static void CopyBuffer(int16_t *pbuffer1, int16_t *pbuffer2, uint16_t BufferSize
   uint32_t i = 0;
   for (i = 0; i < BufferSize; i++)
   {
-    pbuffer1[i] = pbuffer2[i];
+    pbuffer1[i] = pbuffer2[i] / 12;
   }
 }
 
