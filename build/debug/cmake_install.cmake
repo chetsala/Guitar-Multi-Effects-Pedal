23502b22a1e515342f1edfe63b29b9037711dfa3
Chet Sala <chetsala@gmail.com>
2024-10-26 18:29:59 -0400

    Adding in LCD Driver files

--- build/debug/cmake_install.cmake
+++ build/debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Chet/Desktop/ToneEngineers
+# Install script for directory: C:/Users/Chet/Desktop/ToneEngineers/Guitar-Multi-Effects-Pedal
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ToneEngineers")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Guitar-Multi-Effects-Pedal")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Chet/Desktop/ToneEngineers/build/debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Chet/Desktop/ToneEngineers/Guitar-Multi-Effects-Pedal/build/debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
